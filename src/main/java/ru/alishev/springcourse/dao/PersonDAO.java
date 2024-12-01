b302002f3a69498f8b97330c8c6658c4da7d1e16
Sibiryaq <4totakoeaaa@mail.ru>
2024-12-01 22:49:47 +0700

    Spring MVC (Java Code configuration)+ @RequestParam

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private final List<Person> people;
 
     {
         people = new ArrayList<>();
@@ -28,5 +28,10 @@ public class PersonDAO {
         return people.stream()
                 .filter(person -> person.getId() == id)
                 .findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
